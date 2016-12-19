cd153870a055d6c9de045010e82f1d73455b16d7
nehalium <nehal.shah@zalando.de>
2016-12-19 18:31:24 +0100

    added logic and simple unit tests for testing snake case against query params.

--- src/main/java/de/zalando/zally/rules/SnakeCaseForQueryParams.java
+++ src/main/java/de/zalando/zally/rules/SnakeCaseForQueryParams.java
@@ -1,7 +1,53 @@
 package de.zalando.zally.rules;
 
+import de.zalando.zally.Violation;
+import de.zalando.zally.ViolationType;
+import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.QueryParameter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * Created by nshah on 19/12/2016.
+ * Lint for snake case for query params
  */
-public class SnakeCaseForQueryParams {
+class SnakeCaseForQueryParams {
+
+    private static String SNAKE_CASE_PATTERN = "^[a-z0-9_]*$";
+
+    List<Violation> validate(Swagger swagger) {
+        if (swagger == null) return new ArrayList<>();
+
+        List<Violation> violations = new ArrayList<>();
+        swagger.getPaths().forEach((path, pathObject) -> {
+            pathObject.getOperationMap().forEach((httpMethod, operation) -> {
+                operation.getParameters().forEach(param -> {
+                    if (param instanceof QueryParameter) {
+                        if (!isSnakeCase(param.getName())) {
+                            violations.add(getViolation());
+                        }
+                    }
+                });
+            });
+        });
+        return violations;
+    }
+
+    private boolean isSnakeCase(String input) {
+        Pattern pattern = Pattern.compile(SNAKE_CASE_PATTERN);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    private Violation getViolation() {
+        return new Violation(
+            "Must: Use snake_case (never camelCase) for Query Parameters",
+            "Must: Use snake_case (never camelCase) for Query Parameters",
+            ViolationType.MUST,
+            "http://zalando.github.io/restful-api-guidelines/naming/Naming.html#must-use-snakecase-never-camelcase-for-query-parameters"
+        );
+    }
 }
--- src/test/java/de/zalando/zally/rules/SnakeCaseForQueryParamsTest.java
+++ src/test/java/de/zalando/zally/rules/SnakeCaseForQueryParamsTest.java
@@ -1,29 +1,48 @@
 package de.zalando.zally.rules;
 
-import de.zalando.zally.Violation;
 import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
+import org.apache.commons.io.IOUtils;
 import org.junit.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Just an example.
+ * Unit test for snake case for query params
  */
-public class DummyRuleTest {
+public class SnakeCaseForQueryParamsTest {
 
-    private static class DummyRule implements Rule {
+    private Swagger invalidSwagger = getFixture("fixtures/snakeCaseForQueryParamsInvalid.json");
+    private Swagger validSwagger = getFixture("fixtures/snakeCaseForQueryParamsValid.json");
 
-        @Override
-        public List<Violation> validate(Swagger swagger) {
-            return new ArrayList<>();
-        }
+    @Test
+    public void validCase() {
+        assertThat(new SnakeCaseForQueryParams().validate(validSwagger)).isEmpty();
     }
 
     @Test
-    public void dummyRule() {
-        assertThat(new DummyRule().validate(null)).isEmpty();
+    public void invalidCase() {
+        assertThat(new SnakeCaseForQueryParams().validate(invalidSwagger)).isNotEmpty();
+    }
+
+    private Swagger getFixture(String fixture) {
+        SwaggerParser parser = new SwaggerParser();
+        return parser.readWithInfo(readFixtureFile(fixture)).getSwagger();
+    }
+
+    private String readFixtureFile(String fixtureFile) {
+        String result = "";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        try {
+            InputStream is = classloader.getResourceAsStream(fixtureFile);
+            result = IOUtils.toString(is, "UTF-8");
+        }
+        catch (IOException ex) {
+            //TODO: Handle exception
+        }
+        return result;
     }
 }
