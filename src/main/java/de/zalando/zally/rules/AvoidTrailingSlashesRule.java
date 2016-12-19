a51d7f458fb27a4e7180547e38c83269c8b50752
Thomas Abraham <thomas.abraham@zalando-lounge.de>
2016-12-19 17:24:56 +0100

    Added proper test and also added path to violation

--- src/main/java/de/zalando/zally/rules/AvoidTrailingSlashesRule.java
+++ src/main/java/de/zalando/zally/rules/AvoidTrailingSlashesRule.java
@@ -10,7 +10,7 @@ import static de.zalando.zally.ViolationType.MUST;
 
 public class AvoidTrailingSlashesRule implements Rule {
     private static final String TITLE = "Avoid Trailing Slashes";
-    private static final String DESCRIPTION = "Rule avoid trailing slashes is not followed for path ";
+    private static final String DESCRIPTION = "Rule avoid trailing slashes is not followed";
     private static final String RULE_LINK = "http://zalando.github.io/restful-api-guidelines/naming/Naming.html";
 
     @Override
@@ -19,7 +19,7 @@ public class AvoidTrailingSlashesRule implements Rule {
         if (swagger != null && swagger.getPaths() != null) {
             swagger.getPaths().keySet().forEach(path -> {
                 if (path != null && path.trim().endsWith("/")) {
-                    violations.add(new Violation(TITLE, DESCRIPTION + path, MUST, RULE_LINK));
+                    violations.add(new Violation(TITLE, DESCRIPTION, MUST, RULE_LINK, path));
                 }
             });
         }
