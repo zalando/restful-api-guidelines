18459e914083d1c27a1d80f1d058f1e02b57a448
mkulak <mykhailo.kulak@zalando.de>
2016-12-19 18:31:49 +0100

    draft implementation of hyphen headers rule (wip)

--- src/main/java/de/zalando/zally/rules/HyphenateHttpHeadersRule.java
+++ src/main/java/de/zalando/zally/rules/HyphenateHttpHeadersRule.java
@@ -1,15 +1,55 @@
 package de.zalando.zally.rules;
 
 import de.zalando.zally.Violation;
+import de.zalando.zally.ViolationType;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
 import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HyphenateHttpHeadersRule implements Rule {
 
     @Override
     public List<Violation> validate(Swagger swagger) {
+        ArrayList<Violation> res = new ArrayList<>();
+        if (swagger.getParameters() != null) {
+            res.addAll(validate(swagger.getParameters().values()));
+        }
+        for (Path path : swagger.getPaths().values()) {
+            res.addAll(validate(path.getParameters()));
+            for (Operation operation : path.getOperations()) {
+                res.addAll(validate(operation.getParameters()));
+            }
+        }
+        return res;
+    }
 
-        return null;
+    private List<Violation> validate(Collection<Parameter> parameters) {
+        if (parameters == null) {
+            return Collections.emptyList();
+        }
+        return parameters
+                .stream()
+                .filter(p -> p.getIn().equals("header"))
+                .filter(p -> !isHyphenated(p.getName()))
+                .map(this::createViolation)
+                .collect(Collectors.toList());
+    }
+
+    protected boolean isHyphenated(String s) {
+        return false;
+    }
+
+    private Violation createViolation(Parameter p) {
+        return new Violation("Must: Use Hyphenated HTTP Headers",
+                "Header name '" + p.getName() + "' is not hyphenated",
+                ViolationType.MUST,
+                "http://zalando.github.io/restful-api-guidelines/naming/Naming.html");
     }
 }
--- src/test/java/de/zalando/zally/rules/HyphenateHttpHeadersRuleTest.java
+++ src/test/java/de/zalando/zally/rules/HyphenateHttpHeadersRuleTest.java
@@ -1,26 +1,27 @@
 package de.zalando.zally.rules;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.zalando.zally.Violation;
 import de.zalando.zally.ViolationType;
 import io.swagger.models.Swagger;
 import io.swagger.models.parameters.HeaderParameter;
 import io.swagger.models.parameters.Parameter;
 import io.swagger.parser.SwaggerParser;
-import io.swagger.parser.util.DeserializationUtils;
-import io.swagger.util.Json;
 import org.junit.Test;
 
-import java.io.InputStream;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class HyphenateHttpHeadersRuleTest {
     @Test
     public void emptySwaggerShouldPass() {
-        assertThat(new HyphenateHttpHeadersRule().validate(new Swagger())).isEmpty();
+        Swagger swagger = new Swagger();
+        swagger.setParameters(new HashMap<>());
+        assertThat(new HyphenateHttpHeadersRule().validate(swagger)).isEmpty();
     }
 
     @Test
@@ -28,13 +29,14 @@ public class HyphenateHttpHeadersRuleTest {
         Swagger swagger = new Swagger();
         HashMap<String, Parameter> parameters = new HashMap<>();
         HeaderParameter parameter = new HeaderParameter();
-        parameters.put("foo", parameter);
+        parameter.setName("foo");
+        parameters.put(parameter.getName(), parameter);
         swagger.setParameters(parameters);
         List<Violation> result = new HyphenateHttpHeadersRule().validate(swagger);
-        assertThat(result).isEqualTo(Collections.singletonList(
+        assertThat(result).hasSameElementsAs(Collections.singletonList(
                 new Violation(
                         "Must: Use Hyphenated HTTP Headers",
-                        "Parameter name 'foo' is not hyphenated",
+                        "Header name 'foo' is not hyphenated",
                         ViolationType.MUST,
                         "http://zalando.github.io/restful-api-guidelines/naming/Naming.html")
                 )
@@ -51,5 +53,20 @@ public class HyphenateHttpHeadersRuleTest {
     public void positiveCaseTinbox() {
         Swagger swagger = new SwaggerParser().read("api_tinbox.yaml");
         assertThat(new HyphenateHttpHeadersRule().validate(swagger)).isEmpty();
+    }
+
+    @Test
+    public void hyphenationCheck() {
+        HyphenateHttpHeadersRule rule = new HyphenateHttpHeadersRule();
+        assertTrue(rule.isHyphenated(""));
+        assertTrue(rule.isHyphenated("A"));
+        assertTrue(rule.isHyphenated("Aa"));
+        assertFalse(rule.isHyphenated("aA"));
+        assertFalse(rule.isHyphenated("AA"));
+        assertTrue(rule.isHyphenated("A-A"));
+        assertTrue(rule.isHyphenated("This-Is-Some-Hyphenated-String"));
+        assertTrue(rule.isHyphenated("this-is-other-hyphenated-string"));
+        assertFalse(rule.isHyphenated("Sorry no hyphens here"));
+        assertFalse(rule.isHyphenated("CamelCaseIsNotAcceptableAndShouldBeIllegal"));
     }
 }
