b5a84ecd6aade94aef04f646cf95032ea93c8637
Thomas Abraham <methomasabraham@gmail.com>
2016-12-20 12:25:05 +0100

    Made some progress in validating the rule

--- src/main/java/de/zalando/zally/rules/DefineOAuthScopesRule.java
+++ src/main/java/de/zalando/zally/rules/DefineOAuthScopesRule.java
@@ -1,16 +1,77 @@
 package de.zalando.zally.rules;
 
 import de.zalando.zally.Violation;
+import io.swagger.models.Operation;
 import io.swagger.models.Swagger;
+import io.swagger.models.auth.OAuth2Definition;
+import io.swagger.models.auth.SecuritySchemeDefinition;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static de.zalando.zally.ViolationType.MUST;
+import static java.lang.String.format;
 
 public class DefineOAuthScopesRule implements Rule {
+    private static final String TITLE = "Define and Assign Access Rights (Scopes)";
+    private static final String NOT_DEFINED_DESC = "No valid OAuth2 scope is defined for endpoint %s";
+    private static final String INVALID_SCOPE_DESC = "Invalid OAuth2 scope %s for the endpoint %s";
+    private static final String RULE_LINK = "http://zalando.github.io/restful-api-guidelines/security/Security.html";
+    static final String OAUTH2 = "oauth2";
+
     @Override
     public List<Violation> validate(Swagger swagger) {
         List<Violation> violations = new ArrayList<>();
-        // TODO implement validation of this rule
+
+        if (swagger == null || swagger.getPaths() == null) {
+            return violations;
+        }
+
+        Map<String, String> scopes = getScopes(swagger);
+
+        swagger.getPaths().forEach((pathKey, path) -> {
+            if (path != null) {
+                path.getOperations().forEach(operation -> {
+                    violations.addAll(validateOperation(operation, pathKey));
+                });
+            }
+        });
         return violations;
+    }
+
+    // Validate one endpoint
+    private List<Violation> validateOperation(Operation operation, String path) {
+        List<Violation> violations = new ArrayList<>();
+        List<Map<String, List<String>>> security = operation.getSecurity();
+        boolean validScopePresent = false;
+        if (security != null || !security.isEmpty()) {
+            // TODO check if valid scope is present
+            // TODO check if all scopes present is valid
+        }
+        if (!validScopePresent) {
+            String description = format(NOT_DEFINED_DESC, operation.getSummary());
+            violations.add(createViolation(path, description));
+        }
+        return violations;
+    }
+
+    // create a violation for an operation under a path
+    private Violation createViolation(String path, String desc) {
+        return new Violation(TITLE, desc, MUST, RULE_LINK, path);
+    }
+
+    // get the scopes from security definition
+    private Map<String, String> getScopes(Swagger swagger) {
+        Map<String, String> scopes = null;
+        if (swagger.getSecurityDefinitions() != null) {
+            Map<String, SecuritySchemeDefinition> securityDefinitions = swagger.getSecurityDefinitions();
+            SecuritySchemeDefinition securitySchemeDefinition = securityDefinitions.get(OAUTH2);
+            if (securitySchemeDefinition != null && securitySchemeDefinition instanceof OAuth2Definition) {
+                OAuth2Definition oAuth2Definition = (OAuth2Definition) securitySchemeDefinition;
+                scopes = oAuth2Definition.getScopes();
+            }
+        }
+        return scopes;
     }
 }
