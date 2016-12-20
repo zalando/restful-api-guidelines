44267b6c860de100f71eb4288da965cf51d1c1a3
Mikhail Chernykh <mikhail.chernykh@zalando.de>
2016-12-20 19:24:41 +0100

    cli: Read JSON file from command

--- cli/src/main/java/de/zalando/zally/cli/Main.java
+++ cli/src/main/java/de/zalando/zally/cli/Main.java
@@ -2,6 +2,10 @@ package de.zalando.zally.cli;
 
 import com.github.ryenus.rop.OptionParser;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
 
 @OptionParser.Command(name = "zally", descriptions = "Lints the given swagger file using Zally service")
 public class Main {
@@ -11,6 +15,35 @@ public class Main {
     }
 
     void run(String[] args) {
-        // TODO: Implement command
+        try {
+            lint(args);
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private void lint(String[] args) throws RuntimeException {
+        if (args.length < 1) {
+            throw new RuntimeException("Please provide a swagger file");
+        }
+
+        Reader fileReader = getReader(args[0]);
+        JsonReader jsonReader = new JsonReader(fileReader);
+        RequestDecorator decorator = new RequestDecorator(jsonReader);
+
+        String body = decorator.getRequestBody();
+
+        // TODO: add API call
+        System.out.println(body);
+    }
+
+    private Reader getReader(String location) throws RuntimeException {
+        try {
+            return new FileReader(location);
+        } catch (FileNotFoundException e) {
+            String message = "File " + location + " is not found";
+            throw new RuntimeException(message);
+        }
     }
 }
