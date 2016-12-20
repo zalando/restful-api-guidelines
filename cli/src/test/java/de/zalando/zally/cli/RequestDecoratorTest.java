84429ec1f6ca7dd9a5c13ff62275214a4c9e30be
Mikhail Chernykh <mikhail.chernykh@zalando.de>
2016-12-20 19:22:46 +0100

    cli: Add RequestDecorator

---
+++ cli/src/test/java/de/zalando/zally/cli/RequestDecoratorTest.java
@@ -0,0 +1,30 @@
+package de.zalando.zally.cli;
+
+import com.eclipsesource.json.JsonObject;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.assertEquals;
+
+public class RequestDecoratorTest {
+    public final static String fixture = "{\"a\":1,\"b\":\"test\"}";
+    public final static String expectedResult = "{\"api_object\":" + fixture + "}";
+
+    @Test
+    public void wrapsTheResult() {
+        SpecsReader reader = getReader();
+
+        RequestDecorator decorator = new RequestDecorator(reader);
+        String result = decorator.getRequestBody();
+
+        assertEquals(expectedResult, result);
+    }
+
+    private SpecsReader getReader() {
+        InputStream inputStream = new ByteArrayInputStream(fixture.getBytes());
+        return new JsonReader(new InputStreamReader(inputStream));
+    }
+}
