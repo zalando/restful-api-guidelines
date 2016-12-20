5912f1e8c0e4afb6409d6f3334bc7e8be8fc1b30
Iuri Sisov <iuri.sisov@zalando.de>
2016-12-20 14:43:49 +0100

    fix comments

--- src/main/java/de/zalando/zally/rules/SuccessResponseAsJsonObjectRule.java
+++ src/main/java/de/zalando/zally/rules/SuccessResponseAsJsonObjectRule.java
@@ -2,12 +2,16 @@ package de.zalando.zally.rules;
 
 import de.zalando.zally.Violation;
 import de.zalando.zally.ViolationType;
-import io.swagger.models.*;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Path;
+import io.swagger.models.Response;
+import io.swagger.models.Swagger;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.Map;
+
 
 public class SuccessResponseAsJsonObjectRule implements Rule {
     private static final String TITLE = "Response As JSON Object";
@@ -19,25 +23,18 @@ public class SuccessResponseAsJsonObjectRule implements Rule {
     public List<Violation> validate(Swagger swagger) {
         List<Violation> violations = new ArrayList();
         if (swagger != null && swagger.getPaths() != null) {
-            swagger.getPaths().entrySet().stream().
-                    forEach(path -> path.getValue().getOperationMap().entrySet().stream().
-                            forEach(op -> op.getValue().getResponses().entrySet().stream().
-                                    filter(response -> {
-                                        try {
-                                            Optional optionInt = Optional.of(Integer.valueOf(response.getKey()));
-                                            int httpCodeInt = Integer.valueOf(response.getKey());
-                                            return httpCodeInt >= 200 && httpCodeInt < 300;
-                                        } catch(NumberFormatException nfe) {
-                                            return false;
-                                            // ignore any http codes which are not between 200 and 300
-                                        }
-                                    }).
-                                    filter(response -> response.getValue().getSchema() != null).
-                                    filter(response -> !"ref".equals(response.getValue().getSchema().getType())).
-                                    forEach(response -> {
-                                        String violationPath = path.getKey() + "/" + op.getKey() + "/" + response.getKey();
-                                        violations.add(new Violation(TITLE, DESCRIPTION, VIOLATION_TYPE, RULE_LINK, violationPath));
-                                    })));
+            for (Map.Entry<String, Path> path : swagger.getPaths().entrySet())
+                for (Map.Entry<HttpMethod, Operation> op : path.getValue().getOperationMap().entrySet())
+                    for(Map.Entry<String, Response> response : op.getValue().getResponses().entrySet())
+                        try {
+                            Integer httpCodeInt = Integer.valueOf(response.getKey());
+                            if (httpCodeInt >= 200 && httpCodeInt < 300 && response.getValue().getSchema() != null && !"ref".equals(response.getValue().getSchema().getType())) {
+                                String violationPath = path.getKey() + "/" + op.getKey() + "/" + response.getKey();
+                                violations.add(new Violation(TITLE, DESCRIPTION, VIOLATION_TYPE, RULE_LINK, violationPath));
+                            }
+                        } catch(NumberFormatException nfe) {
+                            // ignore any http codes which are not between 200 and 300
+                        }
 
         }
         return violations;
--- src/test/java/de/zalando/zally/rules/AvoidLinkHeadersRuleTest.java
+++ src/test/java/de/zalando/zally/rules/AvoidLinkHeadersRuleTest.java
@@ -1,12 +1,16 @@
 package de.zalando.zally.rules;
 
+import de.zalando.zally.Violation;
+import de.zalando.zally.ViolationType;
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class AvoidLinkHeadersRuleTest {
 
@@ -28,6 +32,10 @@ public class AvoidLinkHeadersRuleTest {
     public void avoidLinkHeadersInvalidJson() throws IOException {
         AvoidLinkHeadersRule rule = new AvoidLinkHeadersRule();
         Swagger swagger = new SwaggerParser().read("/fixtures/avoidLinkHeaderRuleInvalid.json");
-        assertThat(rule.validate(swagger).size()).isEqualTo(2);
+        List<Violation> violations = rule.validate(swagger);
+        assertThat(violations.size()).isEqualTo(2);
+        assertTrue(violations.stream().allMatch(v -> ViolationType.MUST.equals(v.getViolationType())));
+        assertTrue(violations.stream().allMatch(v -> "Avoid Link in Header Rule".equals(v.getTitle())));
+
     }
 }
