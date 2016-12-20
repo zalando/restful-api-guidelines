2d5cf088b5e4a0a6d32615dc3ea01151eaf12d45
Colin Howald <colin.howald@zalando.de>
2016-12-20 16:22:43 +0100

    minor improvements to common field names rule

--- src/main/java/de/zalando/zally/rules/CommonFieldNamesRule.java
+++ src/main/java/de/zalando/zally/rules/CommonFieldNamesRule.java
@@ -14,7 +14,13 @@ import java.util.Map;
 
 public class CommonFieldNamesRule implements Rule {
 
+    private final String title = "Use common field names";
+    private final String link = "http://zalando.github.io/restful-api-guidelines/common-data-objects/CommonDataObjects.html" +
+            "#must-use-common-field-names";
+    private final ViolationType violationType = ViolationType.MUST;
+
     static final Map<String, Property> commonFields = new HashMap<>();
+
     static {
 
         commonFields.put("id", new StringProperty());
@@ -39,11 +45,6 @@ public class CommonFieldNamesRule implements Rule {
         return expectedFormat == null || expectedFormat.equals(property.getFormat());
     }
 
-    private final String title = "Use common field names";
-    private final String link = "http://zalando.github.io/restful-api-guidelines/common-data-objects/CommonDataObjects.html" +
-            "#must-use-common-field-names";
-    private final ViolationType violationType = ViolationType.MUST;
-
     @Override
     public List<Violation> validate(Swagger swagger) {
         List<Violation> violations = new ArrayList<>();
@@ -55,14 +56,14 @@ public class CommonFieldNamesRule implements Rule {
         for (Map.Entry<String, Model> definitionEntry : swagger.getDefinitions().entrySet()) {
             String definition = definitionEntry.getKey();
             Model model = definitionEntry.getValue();
-            for (Map.Entry<String, Property> propertyEntry: model.getProperties().entrySet()) {
+            for (Map.Entry<String, Property> propertyEntry : model.getProperties().entrySet()) {
                 String name = propertyEntry.getKey();
                 Property property = propertyEntry.getValue();
                 if (!matchesCommonFieldsType(name, property)) {
-                    String description = createDescription(definition, name, "type",property.getType(), commonFields.get(name).getType());
+                    String description = createDescription(definition, name, "type", property.getType(), commonFields.get(name).getType());
                     violations.add(new Violation(title, description, violationType, link));
                 } else if (!matchesCommonFieldsFormat(name, property)) {
-                    String description = createDescription(definition, name, "format",property.getFormat(), commonFields.get(name).getFormat());
+                    String description = createDescription(definition, name, "format", property.getFormat(), commonFields.get(name).getFormat());
                     violations.add(new Violation(title, description, violationType, link));
                 }
             }
