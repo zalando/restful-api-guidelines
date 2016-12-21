d3ebd1339045fade8d6290a47ca8bbc609bf6d7b
Ruben Nagoga <ruben.nagoga@zalando.de>
2016-12-21 16:41:32 +0100

    status post(doesn't work)

--- src/main/java/de/zalando/zally/GithubController.java
+++ src/main/java/de/zalando/zally/GithubController.java
@@ -3,6 +3,9 @@ package de.zalando.zally;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import de.zalando.zally.rules.RulesValidator;
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +17,18 @@ import org.springframework.web.client.RestTemplate;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 public class GithubController {
 
+    private final RulesValidator rulesValidator;
     private final ObjectMapper mapper;
 
     @Autowired
-    public GithubController(ObjectMapper mapper) {
+    public GithubController(RulesValidator rulesValidator, ObjectMapper mapper) {
+        this.rulesValidator = rulesValidator;
         this.mapper = mapper;
     }
 
@@ -39,10 +45,20 @@ public class GithubController {
 
         String swaggerContent = Arrays.stream(swaggerBase64.split("\\n"))
                 .map(this::decodeLine)
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining());
+        Swagger swagger = new SwaggerParser().parse(swaggerContent);
 
-        System.out.println(swaggerContent);
+        List<Violation> violations = rulesValidator.validate(swagger);
 
+        String statusUrl = String.format("%s/statuses/%s", repoUrl, commitSha);
+        System.out.println("statusUrl = " + statusUrl);
+        ObjectNode status = mapper.createObjectNode()
+                .put("state", "pending")
+                .put("target_url", "http://example.org")
+                .put("description", "Pending!!!")
+                .put("context", "Swagger Linter");
+        ResponseEntity<JsonNode> statusResponse = client.postForEntity(statusUrl, status, JsonNode.class);
+        System.out.println("statusResponse = " + statusResponse);
         ObjectNode response = mapper.createObjectNode().put("status", swaggerContent);
         return ResponseEntity.ok(response);
     }
