fb2692b30c0680e4855aaf5c655fb6da9296e3a5
mkulak <mykhailo.kulak@zalando.de>
2016-12-21 15:59:32 +0100

    cosmetics

--- src/main/java/de/zalando/zally/GithubController.java
+++ src/main/java/de/zalando/zally/GithubController.java
@@ -2,10 +2,8 @@ package de.zalando.zally;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +14,6 @@ import org.springframework.web.client.RestTemplate;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
@@ -32,35 +28,28 @@ public class GithubController {
 
     @RequestMapping(value = "/github-hook", method = RequestMethod.POST)
     public ResponseEntity<JsonNode> githubHook(@RequestBody JsonNode payload) {
+        String repoUrl = payload.get("repository").get("url").asText();
         String commitSha = payload.get("pull_request").get("head").get("sha").asText();
+        String pathToFile = "swagger/swagger.yaml";
+        String swaggerUrl = String.format("%s/contents/%s?ref=%s", repoUrl, pathToFile, commitSha);
 
         RestTemplate client = new RestTemplate();
-
-        String pathToFile = "swagger/swagger.yaml";
-
-        String repoUrl = payload.get("repository").get("url").asText();
-        String swaggerUrl = String.format("%s/contents/%s?ref=%s",
-                repoUrl, pathToFile, commitSha);
-
         JsonNode swaggerNode = client.getForEntity(swaggerUrl, JsonNode.class).getBody();
         String swaggerBase64 = swaggerNode.get("content").asText();
 
         String swaggerContent = Arrays.stream(swaggerBase64.split("\\n"))
-                .map(line -> Base64.getDecoder().decode(line))
                 .map(this::decodeLine)
                 .collect(Collectors.joining("\n"));
 
         System.out.println(swaggerContent);
 
-        ObjectNode response = mapper.createObjectNode();
-        response.put("status", swaggerContent);
-        System.out.println(payload);
+        ObjectNode response = mapper.createObjectNode().put("status", swaggerContent);
         return ResponseEntity.ok(response);
     }
 
-    private String decodeLine(byte[] bytes) {
+    private String decodeLine(String encoded) {
         try {
-            return new String(bytes, "UTF-8");
+            return new String(Base64.getDecoder().decode(encoded), "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
