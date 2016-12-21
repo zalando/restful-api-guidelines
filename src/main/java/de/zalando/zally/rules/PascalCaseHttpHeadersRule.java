f3e0cb54cf0a91bb42aa606253b757bbcfe3e281
Colin Howald <colin.howald@zalando.de>
2016-12-21 12:53:13 +0100

    add new pascal case http headers rule

---
+++ src/main/java/de/zalando/zally/rules/PascalCaseHttpHeadersRule.java
@@ -0,0 +1,27 @@
+package de.zalando.zally.rules;
+
+import de.zalando.zally.Violation;
+import de.zalando.zally.ViolationType;
+import de.zalando.zally.utils.PatternUtil;
+
+import java.util.*;
+
+public class PascalCaseHttpHeadersRule extends HttpHeadersRule {
+    static final String RULE_NAME = "Prefer Hyphenated-Pascal-Case for HTTP header fields";
+    static final String RULE_URL = "http://zalando.github.io/restful-api-guidelines/naming/Naming.html" +
+            "#should-prefer-hyphenatedpascalcase-for-http-header-fields";
+    static final String DESCRIPTION = "Header name '%s' is not Hyphenated-Pascal-Case";
+
+    @Override
+    Violation createViolation(String header, Optional<String> path) {
+        if (path.isPresent()) {
+            return new Violation(RULE_NAME, String.format(DESCRIPTION, header), ViolationType.SHOULD, RULE_URL, path.get());
+        }
+        return new Violation(RULE_NAME, String.format(DESCRIPTION, header), ViolationType.SHOULD, RULE_URL);
+    }
+
+    @Override
+    boolean isViolation(String header) {
+        return !PatternUtil.isHyphenatedPascalCase(header);
+    }
+}
