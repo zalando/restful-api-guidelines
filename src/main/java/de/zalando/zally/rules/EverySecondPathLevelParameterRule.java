6dfa61b4fb6361e2f5ffcd71027e85cd8dee4eb0
Iuri Sisov <iuri.sisov@zalando.de>
2016-12-21 12:10:58 +0100

    new rule every second element to be path parameter

---
+++ src/main/java/de/zalando/zally/rules/EverySecondPathLevelParameterRule.java
@@ -0,0 +1,42 @@
+package de.zalando.zally.rules;
+
+import de.zalando.zally.Violation;
+import de.zalando.zally.ViolationType;
+import de.zalando.zally.utils.PatternUtil;
+import io.swagger.models.*;
+import io.swagger.models.parameters.Parameter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class EverySecondPathLevelParameterRule implements Rule {
+    private static final String TITLE = "Every Second Path Level To Be Parameter";
+    private static final String DESCRIPTION = "Every second path level must be a path parameter";
+    private static final ViolationType VIOLATION_TYPE = ViolationType.MUST;
+    private static final String RULE_LINK = null;
+
+    @Override
+    public List<Violation> validate(Swagger swagger) {
+        List<Violation> violations = new ArrayList<>();
+        if (swagger == null || swagger.getPaths() == null) {
+            return violations;
+        }
+        for (String path : swagger.getPaths().keySet()) {
+            String[] pathSegments = path.split("/");
+            // we are only interested about every second element starting from the third element after split
+            for (int i = 2; i < pathSegments.length; i += 2) {
+                if (!PatternUtil.isPathVariable(pathSegments[i])){
+                    violations.add(createViolation(path));
+                    break;
+                }
+            }
+        }
+        return violations;
+    }
+
+    private Violation createViolation(String path) {
+        return new Violation(TITLE, DESCRIPTION, VIOLATION_TYPE, RULE_LINK, path);
+    }
+}
