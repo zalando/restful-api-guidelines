7e02bfea79e9cb3e00c2f3a5ca36e15cbf4f803b
Felix Müller <felix.mueller2@zalando.de>
2016-12-23 14:42:00 +0100

    added api exposing via twintip api discovery endpoints

--- src/main/java/de/zalando/zally/Application.java
+++ src/main/java/de/zalando/zally/Application.java
@@ -1,12 +1,12 @@
 package de.zalando.zally;
 
-import io.swagger.models.Swagger;
-import io.swagger.parser.SwaggerParser;
-import io.swagger.util.Json;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.zalando.twintip.spring.SchemaResource;
 
 @SpringBootApplication
+@Import(SchemaResource.class)
 public class Application {
 
     public static void main(String[] args) {
