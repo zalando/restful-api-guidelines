bd82e98d81a2579867673d05f088946f49ef5be0
Felix Müller <felix.mueller.berlin@googlemail.com>
2017-01-02 17:21:49 +0100

    added support for insecure https support (#74)

--- cli/src/main/java/de/zalando/zally/cli/ZallyApiClient.java
+++ cli/src/main/java/de/zalando/zally/cli/ZallyApiClient.java
@@ -5,8 +5,39 @@ import com.eclipsesource.json.JsonValue;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class ZallyApiClient {
+
+    static {
+        try {
+            SSLContext ignoreSslChecks = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy() {
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+
+            CloseableHttpClient unsafeHttpClient = HttpClients.custom().setSSLContext(ignoreSslChecks)
+                    .setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
+
+            Unirest.setHttpClient(unsafeHttpClient);
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            System.out.println("Could not disable verification of ssl certificates. "
+                    + "Support for linter services available through HTTPS can be limited.");
+        }
+    }
+
     private final String url;
     private final String token;
 
