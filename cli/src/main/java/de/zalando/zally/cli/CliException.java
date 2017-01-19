106682ccd2fe4ef2151b9ca95662efbe4c73cdb4
Mikhail Chernykh <mikhail.chernykh@zalando.de>
2017-01-19 19:01:55 +0100

    cli: Add CliException and CliExceptionType

---
+++ cli/src/main/java/de/zalando/zally/cli/CliException.java
@@ -0,0 +1,41 @@
+package de.zalando.zally.cli;
+
+public class CliException extends RuntimeException {
+    private String title;
+    private String details;
+    private CliExceptionType type;
+
+    public CliException(CliExceptionType type, String title, String details) {
+        this.type = type;
+        this.title = title;
+        this.details = details;
+    }
+
+    @Override
+    public String getMessage() {
+        StringBuilder result = new StringBuilder();
+
+        if (title != null && !title.isEmpty()) {
+            result.append(getType());
+            result.append(title);
+        }
+
+        if (details != null && !details.isEmpty()) {
+            result.append("\n\n");
+            result.append(details);
+        }
+
+        return result.toString();
+    }
+
+    private String getType() {
+        switch (type) {
+            case API:
+                return "API: ";
+            case PARAMETERS:
+                return "Command-line Parameters: ";
+            default:
+                return "";
+        }
+    }
+}
