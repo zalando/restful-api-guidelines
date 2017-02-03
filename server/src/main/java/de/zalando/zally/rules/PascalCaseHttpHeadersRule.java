e76b7120bcb583e7bb3b0383792c4443849da808
Mikhail Chernykh <mikhail.chernykh@zalando.de>
2017-02-03 18:33:33 +0100

    server: Cleanup PascalCaseHttpHeadersRule

--- server/src/main/java/de/zalando/zally/rules/PascalCaseHttpHeadersRule.java
+++ server/src/main/java/de/zalando/zally/rules/PascalCaseHttpHeadersRule.java
@@ -9,10 +9,10 @@ import java.util.Optional;
 
 @Component
 public class PascalCaseHttpHeadersRule extends HttpHeadersRule {
-    static final String RULE_NAME = "Prefer Hyphenated-Pascal-Case for HTTP header fields";
-    static final String RULE_URL = "http://zalando.github.io/restful-api-guidelines/naming/Naming.html" +
+    public static final String RULE_NAME = "Prefer Hyphenated-Pascal-Case for HTTP header fields";
+    public static final String RULE_URL = "http://zalando.github.io/restful-api-guidelines/naming/Naming.html" +
             "#should-prefer-hyphenatedpascalcase-for-http-header-fields";
-    static final String DESCRIPTION = "Header name '%s' is not Hyphenated-Pascal-Case";
+    public static final String DESCRIPTION = "Header name '%s' is not Hyphenated-Pascal-Case";
 
     @Override
     public Violation createViolation(String header, Optional<String> path) {
