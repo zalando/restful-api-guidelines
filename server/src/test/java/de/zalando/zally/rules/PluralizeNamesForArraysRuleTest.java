602215a294bd6883def86e52a6586b89acabeba5
Mikhail Chernykh <netmisch@gmail.com>
2017-02-06 19:24:01 +0100

    server: Clean up PluralizeNamesForArraysRuleTest

--- server/src/test/java/de/zalando/zally/rules/PluralizeNamesForArraysRuleTest.java
+++ server/src/test/java/de/zalando/zally/rules/PluralizeNamesForArraysRuleTest.java
@@ -2,11 +2,11 @@ package de.zalando.zally.rules;
 
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
@@ -16,9 +16,14 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PluralizeNamesForArraysRuleTest {
-    private Swagger validSwagger = getFixture("fixtures/pluralizeArrayNamesValid.json");
-    private Swagger invalidSwagger = getFixture("fixtures/pluralizeArrayNamesInvalid.json");
+    private Swagger validSwagger;
+    private Swagger invalidSwagger;
 
+    @Before
+    public void setUp() throws Exception {
+        validSwagger = getFixture("fixtures/pluralizeArrayNamesValid.json");
+        invalidSwagger = getFixture("fixtures/pluralizeArrayNamesInvalid.json");
+    }
 
     @Test
     public void positiveCase() {
@@ -36,27 +41,24 @@ public class PluralizeNamesForArraysRuleTest {
     }
 
     @Test
-    public void positiveCaseSpp() {
+    public void positiveCaseSpp() throws Exception {
         Swagger swagger = getFixture("api_spp.json");
         assertThat(new PluralizeNamesForArraysRule().validate(swagger)).isEmpty();
     }
 
     @Test
-    public void positiveCaseTinbox() {
+    public void positiveCaseTinbox() throws Exception {
         Swagger swagger = getFixture("api_tinbox.yaml");
         assertThat(new PluralizeNamesForArraysRule().validate(swagger)).isEmpty();
     }
 
-    private Swagger getFixture(String fixture) {
+    private Swagger getFixture(String fixture) throws IOException {
         try {
             File file = ResourceUtils.getFile("src/test/resources/" + fixture);
             return new SwaggerParser().parse(new String(Files.readAllBytes(file.toPath())));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to read fixture", e);
         } catch (IOException e) {
             e.printStackTrace();
-            throw new RuntimeException("Unable to read fixture", e);
+            throw e;
         }
     }
 }
