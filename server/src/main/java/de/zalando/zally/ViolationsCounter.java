42ae1b034b64784c9f5756f0f8da30afa35610bc
Mikhail Chernykh <netmisch@gmail.com>
2017-03-06 23:26:36 +0100

    server: Use streams in ViolationsCounter #51

--- server/src/main/java/de/zalando/zally/ViolationsCounter.java
+++ server/src/main/java/de/zalando/zally/ViolationsCounter.java
@@ -1,27 +1,19 @@
 package de.zalando.zally;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ViolationsCounter {
-    private final Map<ViolationType, Integer> counters = new HashMap<>();
+    private final Map<ViolationType, Integer> counters;
 
     public ViolationsCounter(List<Violation> violations) {
-        countViolations(violations);
+        counters = violations.
+                stream().
+                collect(Collectors.groupingBy(Violation::getViolationType, Collectors.summingInt(x -> 1)));
     }
 
     public Integer getCounter(ViolationType violationType) {
         return counters.getOrDefault(violationType, 0);
-    }
-
-    private void countViolations(List<Violation> violations) {
-        violations.forEach(violation -> updateCounter(violation));
-    }
-
-    private void updateCounter(Violation violation) {
-        ViolationType violationType = violation.getViolationType();
-        Integer count = counters.getOrDefault(violationType, 0) + 1;
-        counters.put(violationType, count);
     }
 }
