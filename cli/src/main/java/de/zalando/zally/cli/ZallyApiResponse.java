46eb8fb1e2270dca64d7099b682d76830a7ebc01
Mikhail Chernykh <netmisch@gmail.com>
2017-03-13 19:11:35 +0100

    cli: Add ZallyApiResponse wrapper

---
+++ cli/src/main/java/de/zalando/zally/cli/ZallyApiResponse.java
@@ -0,0 +1,46 @@
+package de.zalando.zally.cli;
+
+import com.eclipsesource.json.JsonObject;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ZallyApiResponse {
+    private final JsonObject response;
+
+    private static final String VIOLATIONS = "violations";
+    private static final String MESSAGE = "message";
+    private static final String VIOLATIONS_COUNT = "violations_count";
+
+    public ZallyApiResponse(JsonObject response) {
+        this.response = response;
+    }
+
+    public List<Violation> getViolations() throws CliException {
+        if (response == null || response.get(VIOLATIONS) == null || !response.get(VIOLATIONS).isArray()) {
+            String violationsString = response == null ? null : response.toString();
+            throw new CliException(CliExceptionType.API, "Response JSON is malformed", violationsString);
+        }
+        return response
+                .get(VIOLATIONS)
+                .asArray()
+                .values()
+                .stream()
+                .map(v -> new Violation(v.asObject()))
+                .collect(Collectors.toList());
+    }
+
+    public String getMessage() {
+        if (response == null || !response.names().contains(MESSAGE)) {
+            return null;
+        }
+        return response.get(MESSAGE).asString();
+    }
+
+    public ViolationsCount getCounters() {
+        if (response == null || response.get(VIOLATIONS_COUNT) == null || !response.get(VIOLATIONS_COUNT).isObject()) {
+            throw new CliException(CliExceptionType.API, "Respones does not contain violation counters");
+        }
+        return new ViolationsCount(response.asObject());
+    }
+}
---
+++ cli/src/test/java/de/zalando/zally/cli/ZallyApiResponseTest.java
@@ -0,0 +1,42 @@
+package de.zalando.zally.cli;
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ZallyApiResponseTest {
+    @Test
+    public void returnsListOfViolations() {
+        final JsonArray violations = new JsonArray();
+        final JsonObject firstViolation = new JsonObject();
+        firstViolation.add("title", "Test title");
+        firstViolation.add("description", "Test description");
+        firstViolation.add("violation_type", "MUST");
+        firstViolation.add("rule_link", "http://example.com");
+        firstViolation.add("paths", Json.array());
+
+        final JsonObject secondViolation = new JsonObject();
+        secondViolation.add("title", "Test title");
+        secondViolation.add("violation_type", "MUST");
+        secondViolation.add("description", "Test description");
+        secondViolation.add("rule_link", "http://example.com");
+        secondViolation.add("paths", Json.array());
+
+        violations.add(firstViolation);
+        violations.add(secondViolation);
+
+        final JsonObject jsonResponse = new JsonObject();
+        jsonResponse.add("violations", violations);
+
+        ZallyApiResponse apiResponse = new ZallyApiResponse(jsonResponse);
+
+        List<Violation> result = apiResponse.getViolations();
+        assertEquals(2, result.size());
+    }
+
+}
