3778fbf07476a83e0cd8525a5e571dfc8b59ddb8
Ruben Barilani <ruben.barilani@zalando.de>
2017-03-16 17:16:03 +0100

    web-ui: client read error handling

--- web-ui/src/server/env-handler.js
+++ web-ui/src/server/env-handler.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const env = require('./env');
+const logger = require('./logger');
+const path = require('path');
 
 function buildJS(publicEnv) {
   return `window.process = {}; window.process.env = ${JSON.stringify(publicEnv)}`;
@@ -10,10 +12,21 @@ function envHandler(req, res) {
 
   if(env.ZALANDO_OAUTH && env.CREDENTIALS_DIR) {
     return fs
-      .readFile(`${env.CREDENTIALS_DIR}/client.json`, (err, data) => {
-        if(err) { throw err }
-        const credentials = JSON.parse(data);
-        publicEnv.OAUTH_CLIENT_ID = credentials.client_id;
+      .readFile(path.join(env.CREDENTIALS_DIR, 'client.json'), (err, data) => {
+        if(err) {
+          logger.error('Cannot read client.json!');
+          logger.error(err);
+          res.setHeader('content-type', 'text/javascript');
+          res.write(buildJS(publicEnv));
+          res.end();
+          return;
+        }
+        try {
+          const credentials = JSON.parse(data);
+          publicEnv.OAUTH_CLIENT_ID = credentials.client_id;
+        }catch(error) {
+          logger.error('Cannot parse client.json')
+        }
         res.setHeader('content-type', 'text/javascript');
         res.write(buildJS(publicEnv));
         res.end();
