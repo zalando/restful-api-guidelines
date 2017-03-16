b3decddb8ea743af2e3be1d67474db84707fb291
Ruben Barilani <ruben.barilani@zalando.de>
2017-03-16 18:13:28 +0100

    web-ui: minor update to env-handler

--- web-ui/src/server/env-handler.js
+++ web-ui/src/server/env-handler.js
@@ -7,35 +7,43 @@ function buildJS(publicEnv) {
   return `window.process = {}; window.process.env = ${JSON.stringify(publicEnv)}`;
 }
 
+function sendResponse(res, content) {
+  res.setHeader('content-type', 'text/javascript');
+  res.write(content);
+  res.end();
+}
+
+function zalandoEnvHandler(req, res) {
+  const publicEnv = env.public();
+  fs
+    .readFile(path.join(env.CREDENTIALS_DIR, 'client.json'), (err, data) => {
+      if(err) {
+        logger.error('Cannot read client.json!');
+        logger.error(err);
+        sendResponse(res, buildJS(publicEnv));
+        return;
+      }
+      try {
+        const credentials = JSON.parse(data);
+        publicEnv.OAUTH_CLIENT_ID = credentials.client_id;
+      }catch(error) {
+        logger.error('Cannot parse client.json')
+      }
+      sendResponse(res, buildJS(publicEnv));
+    });
+
+  sendResponse(res, buildJS(publicEnv))
+}
+
 function envHandler(req, res) {
   const publicEnv = env.public();
 
   if(env.ZALANDO_OAUTH && env.CREDENTIALS_DIR) {
-    return fs
-      .readFile(path.join(env.CREDENTIALS_DIR, 'client.json'), (err, data) => {
-        if(err) {
-          logger.error('Cannot read client.json!');
-          logger.error(err);
-          res.setHeader('content-type', 'text/javascript');
-          res.write(buildJS(publicEnv));
-          res.end();
-          return;
-        }
-        try {
-          const credentials = JSON.parse(data);
-          publicEnv.OAUTH_CLIENT_ID = credentials.client_id;
-        }catch(error) {
-          logger.error('Cannot parse client.json')
-        }
-        res.setHeader('content-type', 'text/javascript');
-        res.write(buildJS(publicEnv));
-        res.end();
-      });
+    zalandoEnvHandler(req, res);
+    return;
   }
 
-  res.setHeader('content-type', 'text/javascript');
-  res.write(buildJS(publicEnv));
-  res.end();
+  sendResponse(res, buildJS(publicEnv));
 }
 
 module.exports = envHandler;
