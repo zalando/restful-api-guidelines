71a68a170b21ea7e6a48288a40f75a89b422176c
mkulak <kleshney@gmail.com>
2017-03-17 23:21:26 +0100

    #97 rebased to master

--- server/src/main/java/de/zalando/zally/rules/AvoidSynonymsRule.kt
+++ server/src/main/java/de/zalando/zally/rules/AvoidSynonymsRule.kt
@@ -10,27 +10,28 @@ import org.springframework.stereotype.Component
 @Component
 class AvoidSynonymsRule(@Autowired rulesConfig: Config) : AbstractRule() {
 
-    val TITLE = "Use common property names"
-    val DESC_PATTERN = "Property names should utilize common dictionary"
-    val RULE_URL = ""
+    override val title = "Use common property names"
+    override val url = ""
+    override val violationType = ViolationType.SHOULD
+
+    val descPattern = "Property names should utilize common dictionary"
 
     @Suppress("UNCHECKED_CAST")
-    private val COMMON_DICTIONARY = rulesConfig.getConfig(javaClass.simpleName + ".dictionary")
+    private val commonDictionary = rulesConfig.getConfig(javaClass.simpleName + ".dictionary")
             .entrySet()
             .map { (key, config) -> key to config.unwrapped() as List<String> }
 
     override fun validate(swagger: Swagger): Violation? {
-        val dict = COMMON_DICTIONARY.flatMap { (right, wrongList) -> wrongList.map { it to right } }.toMap()
+        val dict = commonDictionary.flatMap { (canonical, synonyms) -> synonyms.map { it to canonical } }.toMap()
         val res = swagger.definitions.orEmpty().flatMap { (defName, def) ->
-            val props = def?.properties.orEmpty()
-            props.keys.filter { it in dict }.map { it to "#/definitions/$defName" }
+            def?.properties.orEmpty().keys.filter { it in dict }.map { it to "#/definitions/$defName" }
         }
         return if (res.isNotEmpty()) {
             val (names, paths) = res.unzip()
             val details = names.toSet().groupBy(dict::get)
-                    .map { (right, wrongList) -> right + " instead of " + wrongList.joinToString(", ") }
+                    .map { (canonical, synonyms) -> canonical + " instead of " + synonyms.joinToString(", ") }
                     .joinToString("\n")
-            Violation(this, TITLE, "$DESC_PATTERN:\n$details", ViolationType.SHOULD, RULE_URL, paths.toSet().toList())
+            Violation(this, title, "$descPattern:\n$details", violationType, url, paths.toSet().toList())
         } else null
     }
 }
