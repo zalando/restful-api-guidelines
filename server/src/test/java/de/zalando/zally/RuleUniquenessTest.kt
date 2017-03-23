9a8c9468ce804ff2cadc2ef10f62ad62a3441f48
Mikhail Chernykh <netmisch@gmail.com>
2017-03-23 22:41:07 +0100

    server: Add test to validate rule uniqueness #75

---
+++ server/src/test/java/de/zalando/zally/RuleUniquenessTest.kt
@@ -0,0 +1,26 @@
+package de.zalando.zally
+
+import de.zalando.zally.rules.Rule
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootTest
+class RuleUniquenessTest {
+    @Autowired
+    lateinit var rules: List<Rule>
+
+    @Test
+    fun rulesShouldBeUnique() {
+        val duplicatedCodes = rules
+                .groupBy { it.code }
+                .filterValues { it.size > 1 }
+                .keys
+
+        assertEquals("Duplicated rules found: " + duplicatedCodes, 0, duplicatedCodes.count())
+    }
+}
