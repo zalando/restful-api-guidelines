d4b36c3efb7d50b9d20861f6638ac592384eb61c
Mikhail Chernykh <netmisch@gmail.com>
2017-03-30 23:03:50 +0200

    server: Add support for api_definition_url parameter #198

--- server/src/main/java/de/zalando/zally/ApiDefinitionReader.java
+++ server/src/main/java/de/zalando/zally/ApiDefinitionReader.java
@@ -2,6 +2,8 @@ package de.zalando.zally;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import de.zalando.zally.exception.MissingApiDefinitionException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class ApiDefinitionReader {
     private final JsonNode requestBody;
@@ -13,8 +15,17 @@ public class ApiDefinitionReader {
     public String read() throws MissingApiDefinitionException {
         if (requestBody.has("api_definition")) {
             return requestBody.get("api_definition").toString();
+        } else if (requestBody.has("api_definition_url")) {
+            return readFromUrl(requestBody.get("api_definition_url").textValue());
         } else {
             throw new MissingApiDefinitionException();
         }
+    }
+
+    protected String readFromUrl(String url) {
+        // TODO: Add support for yaml files
+        final RestTemplate client = new RestTemplate();
+        final ResponseEntity<JsonNode> response = client.getForEntity(url, JsonNode.class);
+        return response.getBody().toString();
     }
 }
--- server/src/test/java/de/zalando/zally/ApiDefinitionReaderTest.java
+++ server/src/test/java/de/zalando/zally/ApiDefinitionReaderTest.java
@@ -1,14 +1,31 @@
 package de.zalando.zally;
 
+import static net.jadler.Jadler.closeJadler;
+import static net.jadler.Jadler.initJadlerUsing;
+import static net.jadler.Jadler.onRequest;
+import static net.jadler.Jadler.port;
 import static org.junit.Assert.assertEquals;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import de.zalando.zally.exception.MissingApiDefinitionException;
+import net.jadler.stubbing.server.jdk.JdkStubHttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ApiDefinitionReaderTest {
+
+    @Before
+    public void setUp() {
+        initJadlerUsing(new JdkStubHttpServer());
+    }
+
+    @After
+    public void tearDown() {
+        closeJadler();
+    }
 
     @Test(expected=MissingApiDefinitionException.class)
     public void shouldThrowMissingApiDefinitionExceptionWhenDefinitionIsNotFound() {
@@ -23,6 +40,26 @@ public class ApiDefinitionReaderTest {
         assertEquals("{\"swagger\":\"2.0\"}", result);
     }
 
+    @Test
+    public void shouldReadSwaggerDefinitionFromUrl() {
+        final String remotePath = "/test.json";
+        final String url = "http://localhost:" + port() + remotePath;
+        final String responseBody = "{\"swagger\":\"2.0\"}";
+
+        onRequest()
+                .havingMethodEqualTo("GET")
+                .havingPathEqualTo(remotePath)
+                .respond()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody(responseBody);
+
+        final ApiDefinitionReader reader = new ApiDefinitionReader(getJsonNodeWithApiDefinitionUrl(url));
+        final String result = reader.read();
+
+        assertEquals(responseBody, result);
+    }
+
     private JsonNode getJsonNodeWithoutApiDefinition() {
         final ObjectMapper mapper = new ObjectMapper();
         final ObjectNode node = mapper.createObjectNode();
@@ -35,6 +72,13 @@ public class ApiDefinitionReaderTest {
         final ObjectNode node = mapper.createObjectNode();
         final ObjectNode apiDefinitionNode = node.putObject("api_definition");
         apiDefinitionNode.put("swagger", "2.0");
+        return node;
+    }
+
+    private JsonNode getJsonNodeWithApiDefinitionUrl(String url) {
+        final ObjectMapper mapper = new ObjectMapper();
+        final ObjectNode node = mapper.createObjectNode();
+        node.put("api_definition_url", url);
         return node;
     }
 }
