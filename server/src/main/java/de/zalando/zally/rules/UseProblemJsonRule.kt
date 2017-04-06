6d7cc5cd4c2d5073f4cd0a0d1e8d7f38bbb70419
Mikhail Chernykh <netmisch@gmail.com>
2017-04-06 23:05:27 +0200

    server: Verify that errors are referencing to ProblemJson #182

--- server/src/main/java/de/zalando/zally/rules/UseProblemJsonRule.kt
+++ server/src/main/java/de/zalando/zally/rules/UseProblemJsonRule.kt
@@ -2,7 +2,10 @@ package de.zalando.zally.rules
 
 import de.zalando.zally.Violation
 import de.zalando.zally.ViolationType
+import io.swagger.models.Model
+import io.swagger.models.Response
 import io.swagger.models.Swagger
+import io.swagger.models.properties.RefProperty
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,16 +19,18 @@ class UseProblemJsonRule : AbstractRule() {
             "by Client Or Server"
 
     override fun validate(swagger: Swagger): Violation? {
+        val isProblemDefined = isProblemDefinitionCorrect(swagger.definitions.get("Problem"))
         val paths = swagger.paths.orEmpty().flatMap { pathEntry ->
             pathEntry.value.operationMap.orEmpty().flatMap { opEntry ->
                 opEntry.value.responses.orEmpty().flatMap { responseEntry ->
                     val httpCode = responseEntry.key.parseInt()
-                    if (httpCode in 400..599 && responseEntry.value?.schema?.type != "ref") { //TODO MK: implement real check for Problem usage
+                    if (httpCode in 400..599 && (!isProperRef(responseEntry.value) || !isProblemDefined)) {
                         listOf("${pathEntry.key} ${opEntry.key} ${responseEntry.key}")
                     } else emptyList()
                 }
             }
         }
+
         return if (paths.isNotEmpty()) Violation(this, title, DESCRIPTION, violationType, url, paths) else null
     }
 
@@ -35,4 +40,17 @@ class UseProblemJsonRule : AbstractRule() {
             } catch (nfe: NumberFormatException) {
                 null
             }
+
+    private fun isProperRef(response: Response) : Boolean {
+        return if (response.schema?.type == "ref") {
+            val schema : RefProperty = response.schema as RefProperty
+            (schema.`$ref` == "#/definitions/Problem")
+        } else {
+            false
+        }
+    }
+
+    private fun isProblemDefinitionCorrect(definition: Model?) : Boolean {
+        return (definition?.properties?.keys == setOf("type", "title", "status", "detail", "instance"))
+    }
 }
