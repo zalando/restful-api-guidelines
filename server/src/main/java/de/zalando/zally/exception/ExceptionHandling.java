415fee6592a0c00b2ac101fa5093aef410243f8c
Mikhail Chernykh <netmisch@gmail.com>
2017-04-10 22:58:01 +0200

    server: Handle HTTP client status codes properly #247

--- server/src/main/java/de/zalando/zally/exception/ExceptionHandling.java
+++ server/src/main/java/de/zalando/zally/exception/ExceptionHandling.java
@@ -4,10 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.zalando.problem.Problem;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 import org.zalando.problem.spring.web.advice.SpringAdviceTrait;
+
 
 @ControllerAdvice
 public class ExceptionHandling implements ProblemHandling, SpringAdviceTrait {
@@ -16,5 +19,18 @@ public class ExceptionHandling implements ProblemHandling, SpringAdviceTrait {
     public ResponseEntity<Problem> handleMissingApiDefinitionException(MissingApiDefinitionException exception,
                                                                        NativeWebRequest request) {
         return create(HttpStatus.BAD_REQUEST, exception, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleHttpClientErrorException(
+            HttpClientErrorException exception, NativeWebRequest request) {
+        return create(exception.getStatusCode(), exception, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleResourceAccessException(
+            ResourceAccessException exception, NativeWebRequest request) {
+        final String message = "Unknown host: " + exception.getCause().getMessage();
+        return create(HttpStatus.NOT_FOUND, new Exception(message), request);
     }
 }
