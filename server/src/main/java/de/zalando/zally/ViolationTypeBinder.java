3242ecf323c6e4ecda685752dfab7534698e6012
Mikhail Chernykh <netmisch@gmail.com>
2017-04-21 23:32:47 +0200

    server: Add ViolationTypeBinder #262

---
+++ server/src/main/java/de/zalando/zally/ViolationTypeBinder.java
@@ -0,0 +1,12 @@
+package de.zalando.zally;
+
+import java.beans.PropertyEditorSupport;
+import org.springframework.util.StringUtils;
+
+public class ViolationTypeBinder extends PropertyEditorSupport{
+    @Override
+    public void setAsText(final String text) throws IllegalArgumentException {
+        final ViolationType value = StringUtils.hasText(text) ? ViolationType.valueOf(text.toUpperCase()) : null;
+        setValue(value);
+    }
+}
