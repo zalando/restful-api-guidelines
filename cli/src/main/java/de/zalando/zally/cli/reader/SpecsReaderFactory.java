d5fbab9405e23f944df100e24cb89d5eb3ea347b
Mikhail Chernykh <netmisch@gmail.com>
2017-04-28 23:34:55 +0200

    cli: Remove unused imports from SpecsReaderFactory #316

--- cli/src/main/java/de/zalando/zally/cli/reader/SpecsReaderFactory.java
+++ cli/src/main/java/de/zalando/zally/cli/reader/SpecsReaderFactory.java
@@ -1,15 +1,10 @@
 package de.zalando.zally.cli.reader;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import de.zalando.zally.cli.exception.CliException;
 import de.zalando.zally.cli.exception.CliExceptionType;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 
 
