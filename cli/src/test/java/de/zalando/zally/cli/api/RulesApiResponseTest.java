728567af7ced8af09d93eb804dcfc7b3dfec1652
Mikhail Chernykh <netmisch@gmail.com>
2017-05-05 22:40:34 +0200

    cli: Add RulesApiResponse wrapper #299

---
+++ cli/src/test/java/de/zalando/zally/cli/api/RulesApiResponseTest.java
@@ -0,0 +1,42 @@
+package de.zalando.zally.cli.api;
+
+import de.zalando.zally.cli.domain.Rule;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+
+public class RulesApiResponseTest {
+    @Test
+    public void shouldTransformRulesFromJson() throws Exception {
+        JSONArray rulesJson = new JSONArray();
+        rulesJson.put(createJsonRule("First Rule", "M001"));
+        rulesJson.put(createJsonRule("Second Rule", "M002"));
+
+        JSONObject apiResponse = new JSONObject();
+        apiResponse.put("rules", rulesJson);
+
+        final RulesApiResponse response = new RulesApiResponse(apiResponse);
+
+        final List<Rule> rules = response.getRules();
+        final List<String> ruleTitles = rules.stream().map(Rule::getTitle).collect(Collectors.toList());
+
+        Assert.assertEquals(2, rules.size());
+        Assert.assertTrue(ruleTitles.contains("First Rule"));
+        Assert.assertTrue(ruleTitles.contains("Second Rule"));
+    }
+
+    private JSONObject createJsonRule(final String title, final String code) {
+        final JSONObject rule = new JSONObject();
+        rule.put("title", title);
+        rule.put("code", code);
+        rule.put("type", "MUST");
+        rule.put("is_active", true);
+        rule.put("url", "https://example.com");
+        return rule;
+    }
+}
