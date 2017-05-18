45d1a30e7eb25172d101c83c793821516d11f84d
Mikhail Chernykh <netmisch@gmail.com>
2017-05-18 23:14:48 +0200

    server: Define list of allowed HTTP status codes #313

--- server/src/main/java/de/zalando/zally/rules/UseSpecificHttpStatusCodes.kt
+++ server/src/main/java/de/zalando/zally/rules/UseSpecificHttpStatusCodes.kt
@@ -13,6 +13,18 @@ class UseSpecificHttpStatusCodes : AbstractRule() {
     override val violationType = ViolationType.MUST
     override val code = "M016"
 
+    private val allowedStatusCodes = mapOf(
+            "get" to listOf("304"),
+            "post" to listOf("201", "202", "207", "303"),
+            "put" to listOf("201", "202", "204", "303", "409", "412", "415", "423"),
+            "patch" to listOf("202", "303", "409", "412", "415", "423"),
+            "delete" to listOf("202", "204", "303", "409", "412", "415", "423"),
+            "all" to listOf(
+                    "200", "301", "400", "401", "403", "404", "405", "406", "408", "410", "428", "429",
+                    "500", "501", "503"
+            )
+    )
+
     override fun validate(swagger: Swagger): Violation? {
         return null
     }
