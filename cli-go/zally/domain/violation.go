4becb9648c087d4ecea291b4c32e437e2de595a7
Mikhail Chernykh <netmisch@gmail.com>
2017-05-29 22:01:28 +0200

    cli-go: Add Violation.ToString() method #347

--- cli-go/zally/domain/violation.go
+++ cli-go/zally/domain/violation.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // Violation keeps information about Zally violations
 type Violation struct {
 	Title         string   `json:"title"`
@@ -7,4 +12,21 @@ type Violation struct {
 	ViolationType string   `json:"violation_type"`
 	RuleLink      string   `json:"rule_link"`
 	Paths         []string `json:"paths"`
+}
+
+// ToString creates string representation of Violation
+func (v *Violation) ToString() string {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(fmt.Sprintf("%s %s\n", v.ViolationType, v.Title))
+	buffer.WriteString(fmt.Sprintf("\t%s\n", v.Decription))
+	buffer.WriteString(fmt.Sprintf("\t%s\n", v.RuleLink))
+
+	for _, path := range v.Paths {
+		buffer.WriteString(fmt.Sprintf("\t\t%s\n", path))
+	}
+
+	buffer.WriteString("\n")
+
+	return buffer.String()
 }
