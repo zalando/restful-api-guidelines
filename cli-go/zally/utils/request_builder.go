86ddf0b7715785e88d97112895c9771e59978a69
Mikhail Chernykh <netmisch@gmail.com>
2017-05-30 23:57:35 +0200

    cli-go: Add RequestBuilder #347

---
+++ cli-go/zally/utils/request_builder.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+// RequestBuilder builds Zally specific requests
+type RequestBuilder struct {
+	baseURL string
+	token   string
+}
+
+// NewRequestBuilder creates an instance of RequestBuilder
+func NewRequestBuilder(baseURL string, token string) *RequestBuilder {
+	var builder RequestBuilder
+	builder.baseURL = baseURL
+	builder.token = token
+	return &builder
+}
+
+// Build creates HTTP request
+func (r *RequestBuilder) Build(httpVerb string, uri string, body io.Reader) (*http.Request, error) {
+	url, err := r.getAbsoluteURL(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest(httpVerb, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+
+	if len(r.token) > 0 {
+		request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", r.token))
+	}
+
+	return request, nil
+}
+
+func (r *RequestBuilder) getAbsoluteURL(uri string) (string, error) {
+	u, err := url.Parse(r.baseURL)
+	if err != nil {
+		return "", err
+	}
+
+	u.Path = path.Join(u.Path, uri)
+	return u.String(), nil
+}
