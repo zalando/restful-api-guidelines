f59be61db3b2151aca19e30efe55dd4188d546e1
Felix Müller <felix.mueller2@zalando.de>
2017-06-09 11:38:30 +0200

    #244 fix cors support when oauth is enabled

---
+++ server/src/test/java/de/zalando/zally/apireview/RestCorsWithOAuthTest.java
@@ -0,0 +1,29 @@
+package de.zalando.zally.apireview;
+
+import org.junit.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("production")
+public class RestCorsWithOAuthTest extends RestApiBaseTest {
+
+    @Test
+    public void shouldSupportCorsWhenOAuthIsEnabledOnAllResources() {
+        assertThat(optionsRequest(getUrl())).isEqualTo(HttpStatus.OK);
+        assertThat(optionsRequest("/supported-rules")).isEqualTo(HttpStatus.OK);
+        assertThat(optionsRequest("/review-statistics")).isEqualTo(HttpStatus.OK);
+    }
+
+    private HttpStatus optionsRequest(String url) {
+        return restTemplate.exchange(url, HttpMethod.OPTIONS, RequestEntity.EMPTY, String.class).getStatusCode();
+    }
+
+    @Override
+    protected String getUrl() {
+        return "/api-violations";
+    }
+}
