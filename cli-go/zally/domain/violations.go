39bd4630b898afae07e072be5b8d581ec597264e
Mikhail Chernykh <netmisch@gmail.com>
2017-06-12 21:26:59 +0200

    cli-go: Add filtering by violations #362

--- cli-go/zally/domain/violations.go
+++ cli-go/zally/domain/violations.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 // Violations stores list of violations
@@ -26,4 +27,34 @@ func (v *Violations) ToString() string {
 	}
 
 	return buffer.String()
+}
+
+// Must returns must violations
+func (v *Violations) Must() []Violation {
+	return v.filterViolations("MUST")
+}
+
+// Should returns should violations
+func (v *Violations) Should() []Violation {
+	return v.filterViolations("SHOULD")
+}
+
+// May returns may violations
+func (v *Violations) May() []Violation {
+	return v.filterViolations("MAY")
+}
+
+// Hint returns hint violations
+func (v *Violations) Hint() []Violation {
+	return v.filterViolations("HINT")
+}
+
+func (v *Violations) filterViolations(violationType string) []Violation {
+	result := []Violation{}
+	for _, violation := range v.Violations {
+		if strings.ToUpper(violation.ViolationType) == strings.ToUpper(violationType) {
+			result = append(result, violation)
+		}
+	}
+	return result
 }
--- cli-go/zally/domain/violations_test.go
+++ cli-go/zally/domain/violations_test.go
@@ -9,6 +9,44 @@ import (
 )
 
 func TestViolations(t *testing.T) {
+	var mustViolation Violation
+	mustViolation.Title = "Must Title"
+	mustViolation.RuleLink = "http://example.com/mustViolation"
+	mustViolation.ViolationType = "MUST"
+	mustViolation.Decription = "Must Description"
+	mustViolation.Paths = []string{"/path/one", "/path/two"}
+
+	var shouldViolation Violation
+	shouldViolation.Title = "Should Title"
+	shouldViolation.RuleLink = "http://example.com/shouldViolation"
+	shouldViolation.ViolationType = "SHOULD"
+	shouldViolation.Decription = "Should Description"
+	shouldViolation.Paths = []string{"/path/three", "/path/four"}
+
+	var mayViolation Violation
+	mayViolation.Title = "May Title"
+	mayViolation.RuleLink = "http://example.com/mayViolation"
+	mayViolation.ViolationType = "MAY"
+	mayViolation.Decription = "May Description"
+	mayViolation.Paths = []string{"/path/five", "/path/six"}
+
+	var hintViolation Violation
+	hintViolation.Title = "Hint Title"
+	hintViolation.RuleLink = "http://example.com/hintViolation"
+	hintViolation.ViolationType = "HINT"
+	hintViolation.Decription = "Hint Description"
+	hintViolation.Paths = []string{"/path/seven", "/path/eight"}
+
+	var violationsCount ViolationsCount
+	violationsCount.Must = 1
+	violationsCount.Should = 2
+	violationsCount.May = 3
+	violationsCount.Hint = 4
+
+	var violations Violations
+	violations.Violations = []Violation{mustViolation, shouldViolation, mayViolation, hintViolation}
+	violations.ViolationsCount = violationsCount
+
 	t.Run("ToString returns empty string if no violations", func(t *testing.T) {
 		var violations Violations
 		actualResult := violations.ToString()
@@ -17,36 +55,66 @@ func TestViolations(t *testing.T) {
 	})
 
 	t.Run("ToString returns list of violation strings", func(t *testing.T) {
-		var firstViolation Violation
-		firstViolation.Title = "First Title"
-		firstViolation.RuleLink = "http://example.com/firstViolation"
-		firstViolation.ViolationType = "MUST"
-		firstViolation.Decription = "First Description"
-		firstViolation.Paths = []string{"/path/one", "/path/two"}
-
-		var secondViolation Violation
-		firstViolation.Title = "Second Title"
-		firstViolation.RuleLink = "http://example.com/firstViolation"
-		firstViolation.ViolationType = "SHOULD"
-		firstViolation.Decription = "Second Description"
-		firstViolation.Paths = []string{"/path/three", "/path/four"}
-
-		var violationsCount ViolationsCount
-		violationsCount.Must = 1
-		violationsCount.Should = 2
-		violationsCount.May = 3
-		violationsCount.Hint = 4
-
-		var violations Violations
-		violations.Violations = []Violation{firstViolation, secondViolation}
-		violations.ViolationsCount = violationsCount
-
 		actualResult := violations.ToString()
 		expectedResult := fmt.Sprintf(
-			"Violations:\n===========\n\n%s%sSummary:\n========\n\n%s",
-			firstViolation.ToString(),
-			secondViolation.ToString(),
+			"Violations:\n===========\n\n%s%s%s%sSummary:\n========\n\n%s",
+			mustViolation.ToString(),
+			shouldViolation.ToString(),
+			mayViolation.ToString(),
+			hintViolation.ToString(),
 			violationsCount.ToString())
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+	})
+
+	t.Run("filterViolations returns filtered list of MUST violations", func(t *testing.T) {
+		actualResult := violations.filterViolations("MUST")
+		expectedResult := []Violation{mustViolation}
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+	})
+
+	t.Run("filterViolations returns filtered list of SHOULD violations", func(t *testing.T) {
+		actualResult := violations.filterViolations("SHOULD")
+		expectedResult := []Violation{shouldViolation}
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+	})
+
+	t.Run("filterViolations uses case-insensitive type", func(t *testing.T) {
+		actualResult := violations.filterViolations("Should")
+		expectedResult := []Violation{shouldViolation}
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+	})
+
+	t.Run("Must filters by MUST violations", func(t *testing.T) {
+		actualResult := violations.Must()
+		expectedResult := violations.filterViolations("Must")
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+
+	})
+
+	t.Run("Should filters by SHOULD violations", func(t *testing.T) {
+		actualResult := violations.Should()
+		expectedResult := violations.filterViolations("Should")
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+
+	})
+
+	t.Run("May filters by MAY violations", func(t *testing.T) {
+		actualResult := violations.May()
+		expectedResult := violations.filterViolations("May")
+
+		utils.AssertEquals(t, expectedResult, actualResult)
+
+	})
+
+	t.Run("Hint filters by HINT violations", func(t *testing.T) {
+		actualResult := violations.Hint()
+		expectedResult := violations.filterViolations("Hint")
 
 		utils.AssertEquals(t, expectedResult, actualResult)
 	})
