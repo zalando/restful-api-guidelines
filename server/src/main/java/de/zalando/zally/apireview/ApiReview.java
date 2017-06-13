25121d6dd1a45d4359293a9724da08f192be27df
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-06-13 14:56:01 +0200

    #1 setting generation type for db entities

--- server/src/main/java/de/zalando/zally/apireview/ApiReview.java
+++ server/src/main/java/de/zalando/zally/apireview/ApiReview.java
@@ -12,6 +12,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.io.Serializable;
@@ -28,7 +29,7 @@ import java.util.stream.Collectors;
 public class ApiReview implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- server/src/main/java/de/zalando/zally/apireview/RuleViolation.java
+++ server/src/main/java/de/zalando/zally/apireview/RuleViolation.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
@@ -16,7 +17,7 @@ import java.io.Serializable;
 public class RuleViolation implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JsonIgnore
