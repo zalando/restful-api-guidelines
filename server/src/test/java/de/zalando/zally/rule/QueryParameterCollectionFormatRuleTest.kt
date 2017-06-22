0163d9cd4e2d6933c13eb5e16df40a7716d47521
Mario Schlegel <mario.schlegel@zalando.de>
2017-06-22 11:37:28 +0200

    add test cases #336

--- server/src/test/java/de/zalando/zally/rule/QueryParameterCollectionFormatRuleTest.kt
+++ server/src/test/java/de/zalando/zally/rule/QueryParameterCollectionFormatRuleTest.kt
@@ -1,6 +1,8 @@
 package de.zalando.zally.rule
 
 import de.zalando.zally.violation.ViolationType
+import io.swagger.models.Operation
+import io.swagger.models.Path
 import io.swagger.models.Swagger
 import io.swagger.models.parameters.QueryParameter
 import org.assertj.core.api.Assertions
@@ -20,9 +22,33 @@ class QueryParameterCollectionFormatRuleTest {
     }
 
     @Test
+    fun negativeCaseCollectionFormatNotSupportedFromPath() {
+        val paramList = listOf(QueryParameter().apply { name = "test"; type = "array"; collectionFormat = "notSupported" })
+        val swagger = Swagger().apply {
+            paths = mapOf("/apis" to Path().apply { get = Operation().apply { parameters = paramList } })
+        }
+
+        val result = QueryParameterCollectionFormatRule().validate(swagger)!!
+        Assertions.assertThat(result.violationType).isEqualTo(ViolationType.SHOULD)
+        Assertions.assertThat(result.rule.code).isEqualTo("S011")
+    }
+
+    @Test
     fun negativeCaseCollectionFormatNull() {
         val swagger = Swagger().apply {
             parameters = mapOf("test" to QueryParameter().apply { name = "test"; type = "array"; collectionFormat = null })
+        }
+
+        val result = QueryParameterCollectionFormatRule().validate(swagger)!!
+        Assertions.assertThat(result.violationType).isEqualTo(ViolationType.SHOULD)
+        Assertions.assertThat(result.rule.code).isEqualTo("S011")
+    }
+
+    @Test
+    fun negativeCaseCollectionFormatNullFromPath() {
+        val paramList = listOf(QueryParameter().apply { name = "test"; type = "array"; collectionFormat = null })
+        val swagger = Swagger().apply {
+            paths = mapOf("/apis" to Path().apply { get = Operation().apply { parameters = paramList } })
         }
 
         val result = QueryParameterCollectionFormatRule().validate(swagger)!!
@@ -40,9 +66,29 @@ class QueryParameterCollectionFormatRuleTest {
     }
 
     @Test
+    fun positiveCaseCsvFromPath() {
+        val paramList = listOf(QueryParameter().apply { name = "test"; type = "array"; collectionFormat = "csv" })
+        val swagger = Swagger().apply {
+            paths = mapOf("/apis" to Path().apply { get = Operation().apply { parameters = paramList } })
+        }
+
+        Assertions.assertThat(QueryParameterCollectionFormatRule().validate(swagger)).isNull()
+    }
+
+    @Test
     fun positiveCaseMulti() {
         val swagger = Swagger().apply {
             parameters = mapOf("test" to QueryParameter().apply { name = "test"; type = "array"; collectionFormat = "multi" })
+        }
+
+        Assertions.assertThat(QueryParameterCollectionFormatRule().validate(swagger)).isNull()
+    }
+
+    @Test
+    fun positiveCaseMultiFromPath() {
+        val paramList = listOf(QueryParameter().apply { name = "test"; type = "array"; collectionFormat = "multi" })
+        val swagger = Swagger().apply {
+            paths = mapOf("/apis" to Path().apply { get = Operation().apply { parameters = paramList } })
         }
 
         Assertions.assertThat(QueryParameterCollectionFormatRule().validate(swagger)).isNull()
