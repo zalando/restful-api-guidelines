bc4a77903ae4b8d27d54b9fb02f63511bce92e3e
Mikhail Chernykh <netmisch@gmail.com>
2017-06-30 22:09:47 +0200

    cli-go: Filter domain.Rules by type #402

--- cli-go/zally/domain/rules.go
+++ cli-go/zally/domain/rules.go
@@ -1,6 +1,38 @@
 package domain
 
+import "strings"
+
 // Rules stores list of rules
 type Rules struct {
 	Rules []Rule `json:"rules"`
+}
+
+// Must returns must rules
+func (r *Rules) Must() []Rule {
+	return r.filterRules("MUST")
+}
+
+// Should returns should rules
+func (r *Rules) Should() []Rule {
+	return r.filterRules("SHOULD")
+}
+
+// May returns may rules
+func (r *Rules) May() []Rule {
+	return r.filterRules("MAY")
+}
+
+// Hint returns hint rules
+func (r *Rules) Hint() []Rule {
+	return r.filterRules("HINT")
+}
+
+func (r *Rules) filterRules(ruleType string) []Rule {
+	result := []Rule{}
+	for _, rule := range r.Rules {
+		if strings.ToUpper(rule.Type) == strings.ToUpper(ruleType) {
+			result = append(result, rule)
+		}
+	}
+	return result
 }
