c1caed8cee644aba91be9440c8e68ae14cc90b75
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-08-17 10:01:50 +0200

    adding rule_id_counter and uniqueness check script (#260)

---
+++ ASCIIDOC/check_uniqueness_of_rule_ids.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# This script checks if any rule id has been used more than once
+
+CONTENT_DIR=chapters
+
+duplicated_ids=`grep -r -h '^.*\[#[0-9]\{1,5\}.*$' ${CONTENT_DIR} | uniq -d`
+
+if [ -z "$duplicated_ids" ]; then
+    exit 0
+else
+    echo "Duplicated Rule IDs: `echo $duplicated_ids | tr -d '\n'`"
+    exit 1
+fi
