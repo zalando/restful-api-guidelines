9e610da0dfb616dbbc00b1b7ebf0b235647acbc4
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-08-17 09:06:44 +0200

    improving table layout; initial build script (#260)

---
+++ ASCIIDOC/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Script to build Zalando RESTful Guidelines (static HTML, PDF)
+
+set -ex #print commands, exit on failure
+
+BUILD_DIR=docs
+
+rm -rf ${BUILD_DIR}/* #clean
+
+cp -r assets ${BUILD_DIR}/ #copy assets
+
+asciidoctor -D ${BUILD_DIR} index.adoc #generate HTML
+
+asciidoctor-pdf -D ${BUILD_DIR} index.adoc #generate PDF
