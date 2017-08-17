ca755f51f37608b2118c1cda9613e15193a5780e
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-08-17 12:11:42 +0200

    integrating Travis CI (#260)

--- ASCIIDOC/build.sh
+++ ASCIIDOC/build.sh
@@ -3,13 +3,10 @@
 
 set -ex #print commands, exit on failure
 
-BUILD_DIR=docs
+BUILD_DIR=output
 
 rm -rf ${BUILD_DIR}/* #clean
-
 cp -r assets ${BUILD_DIR}/ #copy assets
-
 asciidoctor -D ${BUILD_DIR} index.adoc #generate HTML
-
 asciidoctor-pdf -D ${BUILD_DIR} index.adoc #generate PDF
 asciidoctor-epub3 -D ${BUILD_DIR} index.adoc #generate EPUB3
