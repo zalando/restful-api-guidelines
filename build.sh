f3e05b15f66d087518381fba29514f6fa286011c
Maxim Tschumak <maxim.tschumak@gmail.com>
2017-08-22 10:51:32 +0200

    using asciidoctor docker image in the build process (locally and on travis ci) (#260)

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 # Script to build Zalando RESTful Guidelines (static HTML, PDF)
 
-set -ex #print commands, exit on failure
+set -ex
 
-BUILD_DIR=output
+pushd `dirname $0` > /dev/null
+SCRIPT_DIR=`pwd -P`
+popd > /dev/null
+BUILD_DIR=${SCRIPT_DIR}/output
 
-rm -rf ${BUILD_DIR}/* #clean
-cp -r assets ${BUILD_DIR}/ #copy assets
-cp -r legacy/* ${BUILD_DIR}/ #copy assets
-asciidoctor -D ${BUILD_DIR} index.adoc #generate HTML
-asciidoctor-pdf -D ${BUILD_DIR} index.adoc #generate PDF
-asciidoctor-epub3 -D ${BUILD_DIR} index.adoc #generate EPUB3
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+docker pull asciidoctor/docker-asciidoctor
+
+./check_uniqueness_of_rule_ids.sh
+
+docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor -D /documents/output index.adoc
+docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -D /documents/output index.adoc
+docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 -D /documents/output index.adoc
+
+cp -r assets ${BUILD_DIR}/
+cp -r -n legacy/* ${BUILD_DIR}/
 
 mv ${BUILD_DIR}/index.pdf ${BUILD_DIR}/zalando-restful-guidelines.pdf
 mv ${BUILD_DIR}/index.epub ${BUILD_DIR}/zalando-restful-guidelines.epub
