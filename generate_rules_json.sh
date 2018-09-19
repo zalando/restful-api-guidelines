a5e606e24ee641f24c334cdde69a038b1c84a825
Maxim Tschumak <maxim.tschumak@gmail.com>
2018-09-19 10:30:04 +0200

    feat: generate rules information json in travis build (#446)

---
+++ generate_rules_json.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This script generates a JSON with information about the guideline rules.
+
+build_dir=${1:-output}
+
+grep -Pzoh "(?s)\[(#[0-9]+)\]\n[=]+\s+.*?\n" chapters/* | \
+        sed '$!N;s/\n/ /' | tr -d "[]{}\000"| sed '/^\s*$/d' | \
+        sort | perl -pi -e 'chomp if eof' | \
+        jq -Rs '{rules: split("\n") }' | \
+        jq '{rules: .rules | map(. | {id: split(" == ")[0], title: split(" == ")[1]})}' \
+        > ${build_dir}/rules
