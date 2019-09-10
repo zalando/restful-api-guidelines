e71a73e816974bd1c2f64f6310e0cace7641c257
tkrop <tronje.krop@zalando.de>
2019-09-10 10:50:21 +0200

    feat: reorder chapters and remove duplicate rule (#534)

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoct
 docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -D /documents/output index.adoc
 docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 -D /documents/output index.adoc
 
-cp money.yaml ${BUILD_DIR}/
+cp models/money-1.0.0.yaml ${BUILD_DIR}/
 cp -r assets ${BUILD_DIR}/
 cp -r -n legacy/* ${BUILD_DIR}/
 
