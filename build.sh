3c7cf6a59de789243a4cd0837749628ed10fe8b4
Zeger Knops <zeger.knops@pon.com>
2020-08-07 16:43:09 +0200

    *build script codeacy +link to github +css for rule standard and last-modified +introduction updates +architecture updates

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 set -ex
 
-pushd $(dirname $0) > /dev/null
+pushd $(dirname "$0") > /dev/null
 SCRIPT_DIR=`pwd -P`
 popd > /dev/null
 BUILD_DIR=${SCRIPT_DIR}/output
@@ -14,8 +14,8 @@ docker pull asciidoctor/docker-asciidoctor
 
 ./check_rule_ids.sh
 
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor --verbose --trace -D /documents/output index.adoc
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf --verbose --trace -D /documents/output index.adoc
+docker run -v "${SCRIPT_DIR}:/documents/" asciidoctor/docker-asciidoctor asciidoctor --verbose --trace -D /documents/output index.adoc
+docker run -v "${SCRIPT_DIR}:/documents/" asciidoctor/docker-asciidoctor asciidoctor-pdf --verbose --trace -D /documents/output index.adoc
 # docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 --verbose --trace -D /documents/output index.adoc
 
 cp models/money-1.0.0.yaml "${BUILD_DIR}/"
@@ -24,5 +24,5 @@ cp -r assets "${BUILD_DIR}/"
 
 ./generate_rules_json.sh
 
-mv "${BUILD_DIR}/index.pdf" "${BUILD_DIR}/pon-guidelines.pdf"
+mv "${BUILD_DIR}/index.pdf" "${BUILD_DIR}/pon-guidelines.pdf"   
 # mv ${BUILD_DIR}/index.epub ${BUILD_DIR}/pon-guidelines.epub
