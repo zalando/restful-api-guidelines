715a31563fa3fbc5b49db0f7c252e66c9cf88d29
Zeger Knops <zeger.knops@pon.com>
2020-08-07 14:01:50 +0200

    *codacy build.sh refactor *line breaks in introduction

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 
 set -ex
 
-pushd `dirname $0` > /dev/null
+pushd $(dirname $0) > /dev/null
 SCRIPT_DIR=`pwd -P`
 popd > /dev/null
 BUILD_DIR=${SCRIPT_DIR}/output
 
 rm -rf ${BUILD_DIR}
-mkdir ${BUILD_DIR}
+mkdir "${BUILD_DIR}"
 docker pull asciidoctor/docker-asciidoctor
 
 ./check_rule_ids.sh
@@ -18,11 +18,11 @@ docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoct
 docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf --verbose --trace -D /documents/output index.adoc
 # docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 --verbose --trace -D /documents/output index.adoc
 
-cp models/money-1.0.0.yaml ${BUILD_DIR}/
-cp -r assets ${BUILD_DIR}/
+cp models/money-1.0.0.yaml "${BUILD_DIR}/"
+cp -r assets "${BUILD_DIR}/"
 # cp -r -n legacy/* ${BUILD_DIR}/
 
 ./generate_rules_json.sh
 
-mv ${BUILD_DIR}/index.pdf ${BUILD_DIR}/pon-guidelines.pdf
+mv "${BUILD_DIR}/index.pdf" "${BUILD_DIR}/pon-guidelines.pdf"
 # mv ${BUILD_DIR}/index.epub ${BUILD_DIR}/pon-guidelines.epub
